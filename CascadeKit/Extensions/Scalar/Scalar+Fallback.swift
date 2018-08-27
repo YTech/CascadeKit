deea5447f93a4e83d224f43a9566f1386785fa6d
Ennio Masi <2176586+ennioma@users.noreply.github.com>
2018-08-27 10:49:18 +0100

    Story/minor refactoring (#1)
    * Removed problematic conformance
    
    * Added project license to source files
    
    * Added project license to example project source files

--- CascadeKit/Extensions/Scalar/Scalar+Fallback.swift
+++ CascadeKit/Extensions/Scalar/Scalar+Fallback.swift
@@ -1,6 +1,24 @@
+// MIT License
 //
-//  Copyright © YOOX NET-A-PORTER 2018. All rights reserved.
+// Copyright YOOX NET-A-PORTER (c) 2018
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 import Foundation
 
 public extension Unicode.Scalar {
@@ -8,13 +26,20 @@ public extension Unicode.Scalar {
     ///
     /// - Parameters:
     ///   - alphabets: A collection of Alphabet
-    ///   - isSpecialChar: If the scalar is a special char
+    ///   - isSpecialChar: If the scalar is a special char, it's returned straightaway, instead of
+    ///                    performing a match
     /// - Returns: An Alphabet if exists
     public func match(in alphabets: [Alphabet], isSpecialChar: Bool) -> Alphabet? {
-        guard !alphabets.isEmpty, let firstRange = alphabets.first else {
+        guard
+            !isSpecialChar,
+            !alphabets.isEmpty,
+            let firstRange = alphabets.first else {
+
             return nil
         }
-        if isSpecialChar || firstRange.range ~= value { return firstRange }
+
+        if firstRange.range ~= value { return firstRange }
+
         return match(in: Array(alphabets.dropFirst()), isSpecialChar: isSpecialChar)
     }
 }
