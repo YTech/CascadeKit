27eeb4505623630382f316cb7502691c488b8ea6
Ennio Masi <ennio.masi@gmail.com>
2020-01-23 10:14:05 +0000

    Removed wrong char

--- travis/UnitTest.sh
+++ travis/UnitTest.sh
@@ -4,7 +4,7 @@ set -e
 echo "Running Unit Tests"
 set -o pipefail && xcodebuild -project CascadeKit/CascadeKit.xcodeproj \
 -scheme $CI_BUILD_TARGET \
--destination "platform=iOS Simulator,name=iPhone Xs,OS=§"$OS \
+-destination "platform=iOS Simulator,name=iPhone Xs,OS="$OS \
 -allowProvisioningDeviceRegistration -allowProvisioningUpdates test | xcpretty -s --color --utf
 
 echo " Unit Tests Passed for iPhone"
