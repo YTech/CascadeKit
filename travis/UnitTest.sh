eaf1ce0dac452dccb11b8b04e4a576d2a23df95d
Ennio Masi <ennio.masi@gmail.com>
2020-01-23 10:03:13 +0000

    Fixed a typo about iphone xs

--- travis/UnitTest.sh
+++ travis/UnitTest.sh
@@ -4,7 +4,7 @@ set -e
 echo "Running Unit Tests"
 set -o pipefail && xcodebuild -project CascadeKit/CascadeKit.xcodeproj \
 -scheme $CI_BUILD_TARGET \
--destination "platform=iOS Simulator,name=iPhone XS,OS=§"$OS \
+-destination "platform=iOS Simulator,name=iPhone Xs,OS=§"$OS \
 -allowProvisioningDeviceRegistration -allowProvisioningUpdates test | xcpretty -s --color --utf
 
 echo " Unit Tests Passed for iPhone"
